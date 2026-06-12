9321abe01fb60cad8bb91799eb2798cdc15fed2c
denissisic <denis.sisic@gmail.com>
2026-06-12 15:28:26 +0200

    chore: update deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/codebase-figma-alignment-demo/',
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
